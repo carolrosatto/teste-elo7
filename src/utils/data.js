c6f34a0c3ebc17651fd457b0f25d3a7477b2e754
Carolina Rosatto <carolrosatto@gmail.com>
2023-01-07 17:45:17 -0300

    Adicionando os dados que aparecem na sessão de valores

---
+++ src/utils/data.js
@@ -0,0 +1,17 @@
+const companyValues = [
+  {
+    "icon": "../assets/icons/qualidade.png",
+    "title": "Qualidade de vida",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus condimentum rhoncus cursus. Nunc ac erat consequat, fermentum risus at, lobortis dolor."
+  },
+  {
+    "icon": "../assets/icons/descontracao.png",
+    "title": "Ambiente descontraído",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id auctor turpis, eu dapibus magna. Donec id nulla scelerisque massa feugiat imperdiet."
+  },
+  {
+    "icon": "../assets/icons/atividades.png",
+    "title": "Atividades extras",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet ac lacus eu tincidunt. Phasellus id felis consequat, commodo neque sed, egestas ligula. Quisque vulputate dolor id ornare feugiat."
+  }
+]
