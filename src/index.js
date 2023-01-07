c7ea58d790c18024e8a5ca46e3b705f9a719eb7b
Carolina Rosatto <carolrosatto@gmail.com>
2023-01-07 17:44:50 -0300

    Adicionando google fonts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
+import './global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
