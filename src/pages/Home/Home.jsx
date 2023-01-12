b53916eefeafd09e79be5741360ee418a03a5a3d
Carolina Rosatto <carolrosatto@gmail.com>
2023-01-12 17:30:57 -0300

    Estrutura da pagina Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Banner from '../../components/Banner';
 import Header from '../../components/Header';
+import JobsSection from '../../components/JobsSection';
+import StaffSection from '../../components/StaffSection';
+import VideoSection from '../../components/VideoSection';
+import ValuesSection from '../../components/ValuesSection';
 
 function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <VideoSection />
+      <StaffSection />
+      <ValuesSection />
+      <Banner />
+      <JobsSection />
+    </>
   )
 }
 
