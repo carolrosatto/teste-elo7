47800942284df9753c8d48675c7134e95843a2bf
Carolina Rosatto <carolrosatto@gmail.com>
2023-01-12 17:30:28 -0300

    Estruturando Banner e JobsSection

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <article>
+      <img src="/assets/images/foto-bottom.png" />
+    </article>
+  )
+}
+
+export default Banner;
---
+++ src/components/JobsSection/JobsSection.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from "./JobsSection.module.css";
+
+function Footer() {
+  return (
+    <article>
+      <h2>VAGAS EM ABERTO</h2>
+      <h3>DESENVOLVIMENTO</h3>
+      <table>
+        <tr>
+          <td>Desenvolvedor Mobile Senior</td>
+          <td>Vila Olímpia - SP</td>
+        </tr>
+        <tr>
+          <td>Desenvolvedor Mobile Senior</td>
+          <td>Vila Olímpia - SP</td>
+        </tr>
+        <tr>
+          <td>Desenvolvedor Mobile Senior</td>
+          <td>Vila Olímpia - SP</td>
+        </tr>
+      </table>
+    </article>
+  )
+}
+
+export default Footer;
