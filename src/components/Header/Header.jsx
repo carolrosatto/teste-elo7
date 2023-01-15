84c6eb709580e2108e1d71f5724c9739c1e264ad
Carolina Rosatto <carolrosatto@gmail.com>
2023-01-15 18:19:58 -0300

    Adicionando scroll ao link do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Divider from '../Divider';
-import Link from '../Link';
+import CustomLink from '../Link';
 import styles from './Header.module.css'
 
 function Header() {
+
+  function scrollToItem(element) {
+    if (element !== null) {
+      const item = document.querySelector(element);
+      item.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
   return (
     <header>
       <div className={styles.bannerContainer}>
@@ -12,10 +20,10 @@ function Header() {
       <div className={styles.headerTextContainer}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices mi. Mauris ut volutpat est ipsum.
-          Maecenas vulputate sed diam sed finibus. Phasellus condimentum mollis leo, sit amet commodo dolor imperdiet at.
+          Maecenas vulputate sed diam sed finibus. Phasellus condimentum mollis leo, sit amet commodo dolor imperdiet at.  Curabitur ac ultrices mi.
         </p>
         <Divider />
-        <Link label="VAGAS EM ABERTO" hasIcon href="#" />
+        <CustomLink label="VAGAS EM ABERTO" hasIcon onClick={() => scrollToItem("#jobSection")} />
       </div>
     </header>
   )
